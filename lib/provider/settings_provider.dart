1a4e73f6f17bedf87a261fff3aef53dc92d27930
Narukarudra09 <160457532+Narukarudra09@users.noreply.github.com>
2025-01-20 20:35:08 +0530

    update profile and settings

--- lib/provider/settings_provider.dart
+++ lib/provider/settings_provider.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'auth_provider.dart';
 
 class SettingsProvider with ChangeNotifier {
   bool _notificationsEnabled = true;
-  String _language = 'English';
+  final DatabaseReference _databaseRef =
+      FirebaseDatabase.instance.ref('Feedback');
+  final String _language = 'English';
 
   bool get notificationsEnabled => _notificationsEnabled;
 
@@ -11,5 +17,21 @@ class SettingsProvider with ChangeNotifier {
   void toggleNotifications() {
     _notificationsEnabled = !_notificationsEnabled;
     notifyListeners();
+  }
+
+  Future<void> submitFeedback(String feedback, BuildContext context) async {
+    try {
+      await _databaseRef.child('feedback').push().set({
+        'email': Provider.of<AuthProvider>(context, listen: false)
+            .profile
+            .email
+            .toString(),
+        'message': feedback,
+        'id': DateTime.now().toIso8601String(),
+      });
+      notifyListeners();
+    } catch (error) {
+      throw error;
+    }
   }
 }
--- lib/screens/settings/feedback_screen.dart
+++ lib/screens/settings/feedback_screen.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
-class FeedbackScreen extends StatelessWidget {
+import '../../provider/settings_provider.dart';
+
+class FeedbackScreen extends StatefulWidget {
   const FeedbackScreen({super.key});
+
+  @override
+  State<FeedbackScreen> createState() => _FeedbackScreenState();
+}
+
+class _FeedbackScreenState extends State<FeedbackScreen> {
+  final TextEditingController _feedbackController = TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +21,81 @@ class FeedbackScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Feedback"),
       ),
-      body: ListView(
-        children: [],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Form(
+          key: formKey,
+          child: ListView(
+            children: [
+              const SizedBox(height: 20),
+              TextFormField(
+                keyboardType: TextInputType.text,
+                maxLines: 5,
+                style: GoogleFonts.montserrat(
+                  fontSize: 16,
+                  color: const Color(0xFFF5F5DC),
+                ),
+                controller: _feedbackController,
+                decoration: InputDecoration(
+                  labelText: 'Feedback',
+                  labelStyle:
+                      GoogleFonts.montserrat(color: const Color(0xFFF5F5DC)),
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                        color: Color.fromARGB(255, 21, 27, 31)),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                        color: Color.fromARGB(255, 21, 27, 31)),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                        color: Color.fromARGB(255, 21, 27, 31)),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  filled: true,
+                  fillColor: const Color.fromARGB(255, 21, 27, 31),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a Feedback';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: ElevatedButton(
+                  onPressed: () async {
+                    if (formKey.currentState!.validate()) {
+                      final feedback = _feedbackController.text;
+                      await Provider.of<SettingsProvider>(context,
+                              listen: false)
+                          .submitFeedback(feedback, context);
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Feedback submitted!')),
+                      );
+                      _feedbackController.clear();
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(16),
+                    backgroundColor: const Color.fromARGB(255, 21, 27, 31),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                  child: const Text('Save Changes',
+                      style: TextStyle(color: Color(0xFFF5F5DC))),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
